6f6f9f158c318cf6b2ebc733a45e94c6b7105cb3
Kaur975 <ammmsn@hotmail.co.uk>
2025-12-18 20:36:37 +0000

    Cleaning up unnecessary code

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ require('dotenv/config')
 
 app.use(bodyParser.json())
 
-const filmsRoute = require('./routes/films')
-const authRoute = require('./routes/auth')
-
-app.use('/api/film',filmsRoute)
-app.use('/api/user',authRoute)
-
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/posts", require("./routes/post.routes"));
 app.use("/api/topics", require("./routes/topic.routes"));
